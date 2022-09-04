0a279e8cd7acf37e15b1a035904da7639dd4c3df
Vlad-creator <56085984+Vlad-creator@users.noreply.github.com>
2022-09-05 00:57:35 +0300

    Add files via upload

---
+++ task2/source/graph.c
@@ -0,0 +1,110 @@
+#include "../include/graph.h"
+
+struct node
+{
+	int size;
+	int num;
+	int n_curves;
+	curve_t** out_curves;
+};
+
+struct curve
+{
+	node_t* parent;
+	node_t* child;
+};
+
+struct pair
+{
+	int parent;
+	int child;
+};
+
+void read_pair(pair_t** pair)
+{
+	*pair = (pair_t*)calloc(1 , sizeof(pair_t));
+	scanf("%d" , &((*pair)->parent));
+	scanf("%d" , &((*pair)->child));
+}
+
+void print_pair(pair_t* pair)
+{
+	printf("%d %d" , pair->parent , pair->child);
+}
+
+node_t* create_node(int n , int n_curves_t)
+{
+	node_t* node = (node_t*)calloc(1 , sizeof(node_t));
+	node->out_curves = (curve_t**)calloc(n_curves_t , sizeof(curve_t*));
+	node->num = n;
+	node->size = 0;
+	node->n_curves = n_curves_t;
+	return node;
+}
+
+void add_curve(node_t* node , curve_t* curve)
+{
+	node->out_curves[node->size] = curve;
+	node->size++;
+}
+
+curve_t* create_curve(node_t* parent_t , node_t* child_t)
+{
+	curve_t* curve = (curve_t*)calloc(1 , sizeof(curve_t));
+	curve->parent = parent_t;
+	curve->child = child_t;
+	return curve;
+}
+
+node_t** create_graph(int num_nodes , int num_curves , pair_t** pairs)
+{
+	node_t** nodes = (node_t**)calloc(num_nodes , sizeof(node_t*));
+	int counter = 0;
+	for (int i = 0 ; i < num_nodes ; ++i)
+	{
+		for (int j = 0 ; j < num_curves ; ++j)
+		{
+			if (pairs[j]->parent == i)
+				counter++;
+		}
+		nodes[i] = create_node(i , counter);
+		counter = 0;
+	}
+	for(int i = 0 ; i < num_curves ; ++i)
+	{
+		add_curve(nodes[pairs[i]->parent] , create_curve(nodes[pairs[i]->parent] , nodes[pairs[i]->child]));
+	}
+	return nodes;
+}
+
+void print_graph(node_t** nodes , int num_nodes)
+{
+	for (int i = 0 ; i < num_nodes ; ++i)
+	{
+		printf("%d\n" , nodes[i]->num);
+		for (int j = 0 ; j < nodes[i]->size ; ++j)
+		{
+			printf("%d " , nodes[i]->out_curves[j]->child->num);
+		}
+		printf("\n\n");
+	}
+}
+
+void delete_graph(node_t** nodes , int num_nodes)
+{
+	for (int i = 0 ; i < num_nodes ; ++i)
+	{
+		for (int j = 0 ; j < nodes[i]->size ; ++j)
+		{
+			free(nodes[i]->out_curves[j]);
+		}
+		free(nodes[i]->out_curves);
+		free(nodes[i]);
+	}
+	free(nodes);
+}
+
+/*void BFS(node_t* start , matrix_t* matr)
+{
+
+}*/
