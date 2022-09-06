77d066dcd2b0731a37c521747f8cb1cc31d72236
Vlad-creator <56085984+Vlad-creator@users.noreply.github.com>
2022-09-06 15:29:23 +0300

    Add files via upload

--- task2/main.c
+++ task2/main.c
@@ -9,23 +9,18 @@ int main()
 	scanf("%d" , &num_nodes);
 	scanf("%d" , &num_curves);
 	pair_t** pairs = (pair_t**)calloc(num_curves , sizeof(pair_t*));
+	int* counter = (int*)calloc(num_nodes , sizeof(int));
 	for (int i = 0 ; i < num_curves ; ++i)
 	{
-		read_pair(pairs + i);
+		read_pair(pairs + i , counter);
 	}
-	node_t** nodes = create_graph(num_nodes , num_curves , pairs);
+	node_t** nodes = create_graph(num_nodes , num_curves , pairs , counter);
 	matrix_t* matr = create_matrix(num_nodes , num_nodes);
 	int* global_marks = (int*)calloc(num_nodes , sizeof(int));
 	for (int i = 0 ; i < num_nodes ; ++i)
 	{
 		BFS(nodes[num_nodes - i - 1] , matr , num_nodes , global_marks);
 		global_marks[num_nodes - i - 1] = 1;
-		/*for (int i = 0 ; i < num_nodes ; ++i)
-		{
-			printf("%d " , global_marks[i]);
-		}
-		printf("\n\n");
-		print_matrix(matr);*/
 	}
 	delete_graph(nodes , num_nodes);
 	print_matrix(matr);
