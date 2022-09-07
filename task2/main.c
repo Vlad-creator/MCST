2d185f9bd3125e443432b123abc73fbdb64dc992
Vlad-creator <56085984+Vlad-creator@users.noreply.github.com>
2022-09-07 16:08:40 +0300

    Add files via upload

--- task2/main.c
+++ task2/main.c
@@ -16,15 +16,9 @@ int main()
 	}
 	node_t** nodes = create_graph(num_nodes , num_curves , pairs , counter);
 	matrix_t* matr = create_matrix(num_nodes , num_nodes);
-	int* global_marks = (int*)calloc(num_nodes , sizeof(int));
-	for (int i = 0 ; i < num_nodes ; ++i)
-	{
-		BFS(nodes[num_nodes - i - 1] , matr , num_nodes , global_marks);
-		global_marks[num_nodes - i - 1] = 1;
-	}
+	BFS(nodes[0] , matr , num_nodes);
 	delete_graph(nodes , num_nodes);
 	print_matrix(matr);
 	delete_matrix(matr);
-	free(global_marks);
 	return 0;
 }
